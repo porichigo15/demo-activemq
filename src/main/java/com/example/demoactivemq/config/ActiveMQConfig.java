c6bda4737431b7f85adefca976bbe7b83a0e9631
porichigo15 <por.porkaew15@gmail.com>
2022-04-20 14:56:04 +0700

    set startup jmsTemplate

--- src/main/java/com/example/demoactivemq/config/ActiveMQConfig.java
+++ src/main/java/com/example/demoactivemq/config/ActiveMQConfig.java
@@ -5,6 +5,8 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -27,5 +29,22 @@ public class ActiveMQConfig {
         connectionFactory.setPassword(password);
         connectionFactory.setUserName(user);
         return connectionFactory;
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplate() {
+        JmsTemplate template = new JmsTemplate();
+        template.setConnectionFactory(connectionFactory());
+        template.setPubSubDomain(true);
+        return template;
+    }
+
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory());
+        factory.setPubSubDomain(true);
+        factory.setAutoStartup(true);
+        return factory;
     }
 }
